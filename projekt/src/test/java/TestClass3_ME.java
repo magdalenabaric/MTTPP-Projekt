8fc9528ffd766c92a606b26af4ac6aad221f0053
magdalenabaric <baric.magdalena2@gmail.com>
2024-02-09 19:39:37 +0100

    Adding Test class 3 for MS Edge

---
+++ projekt/src/test/java/TestClass3_ME.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass3_ME {
+
+    public WebDriver driver;
+    //Declare a test URL variable
+    public String testURL = "http://www.google.com";
+    //----------------------Test Setup-----------------------------------
+    @BeforeMethod
+    public void setupTest() {
+        System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.navigate().to(testURL);
+    }
+    @Test
+    public void googleSearchTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        WebElement searchTextBox = driver.findElement(By.name("q"));
+        searchTextBox.sendKeys("parfemi milano");
+        searchTextBox.submit();
+        Thread.sleep(5000);
+        WebElement testLink =
+                driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/div/div/span")
+                );
+
+        Assert.assertEquals(testLink.getText(), "Parfemi Milano");
+        System.out.print(testLink.getText());
+    }
+    @AfterMethod
+    public void teardownTest() {
+        driver.quit();
+    }
+}
