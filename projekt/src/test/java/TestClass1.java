2d40910ff075492a77650c70d2b2c89dce8dcd8a
magdalenabaric <baric.magdalena2@gmail.com>
2024-02-09 14:41:02 +0100

    Adding Test class

---
+++ projekt/src/test/java/TestClass1.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class TestClass1 {
+    //-------------------Global Variables-----------------------------------
+    //Declare a Webdriver variable
+    public WebDriver driver;
+    //Declare a test URL variable
+    public String testURL = "http://www.google.com";
+    //----------------------Test Setup-----------------------------------
+    @BeforeMethod
+    public void setupTest() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+    //Create a new ChromeDriver
+        driver = new ChromeDriver();
+    //Go to www.swtestacademy.com
+        driver.navigate().to(testURL);
+    }
+    @Test
+    public void googleSearchTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        WebElement searchTextBox = driver.findElement(By.name("q"));
+        searchTextBox.sendKeys("profumi milano HR");
+        searchTextBox.submit();
+        Thread.sleep(5000);
+        WebElement testLink =
+                driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")
+                );
+
+        Assert.assertEquals(testLink.getText(), "Profumi Milano -> Visokokvalitetni, pristupačni mirisi ...");
+        System.out.print(testLink.getText());
+    }
+    //---------------Test TearDown-----------------------------------
+    @AfterMethod
+    public void teardownTest() {
+//Close browser and end the session
+        driver.quit();
+    }
+
+}
